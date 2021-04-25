c8ae757c4f6fa796841eb2187ea3c16ea08c731c
effprime <exdoubledot@gmail.com>
2021-04-25 19:27:26 -0400

    refactor, add more endpoints

--- main.py
+++ main.py
@@ -9,30 +9,88 @@ from discord.ext import ipc
 if not os.getenv("IPC_SECRET"):
     raise RuntimeError("IPC_SECRET env not found")
 
-ipc = ipc.Client(secret_key=os.getenv("IPC_SECRET"))
+
+class JSONResponse(quart.Response):
+    """Response class that defaults to JSON mimetype.
+    """
+    default_mimetype = "application/json"
+
+
+class IPCClient(ipc.Client):
+    """Expanded IPC client.
+    """
+
+    async def request(self, *args, **kwargs):
+        """Returns the request status code as a second element.
+        """
+        response = await super().request(*args, **kwargs)
+        code = response.get("code", 200)
+        return response, code
+
+
+ipc = IPCClient(secret_key=os.getenv("IPC_SECRET"))
 app = quart.Quart(__name__)
+app.response_class = JSONResponse
 
 
 @app.route("/health")
 async def health():
     """Runs a health check request on the bot."""
-    health_status = await ipc.request("health")
-    return str(health_status)
+    response = await ipc.request("health")
+    return response
 
 
 @app.route("/describe")
 async def describe():
     """Runs a describe request on the bot."""
-    bot_data = await ipc.request("describe")
-    return str(bot_data)
+    response = await ipc.request("describe")
+    return response
 
 
-@app.route("/config")
+@app.route("/config", methods=["GET", "PUT"])
 async def config():
     """Runs a config request."""
     guild_id = quart.request.args.get("guild_id")
-    config_ = await ipc.request("config", guild_id=guild_id)
-    return str(config_)
+
+    if quart.request.method == "GET":
+        if guild_id:
+            response = await ipc.request("get_guild_config", guild_id=guild_id)
+        else:
+            response = await ipc.request("get_bot_config")
+
+    if quart.request.method == "PUT":
+        new_config = await quart.request.json
+        response = await ipc.request(
+            "edit_guild_config", guild_id=guild_id, new_config=new_config
+        )
+
+    return response
+
+
+@app.route("/plugin/status")
+async def plugin_status():
+    """Runs a describe request on the bot."""
+    response = await ipc.request("get_plugin_status")
+    return response
+
+
+@app.route("/plugin/<action>/<plugin_name>")
+async def plugin_action(action, plugin_name):
+    """Runs a plugin action on a plugin."""
+    # clever girl
+    response = await ipc.request(f"{action}_plugin", plugin_name=plugin_name)
+    return response
+
+
+@app.errorhandler(Exception)
+async def handle_exception(exception):
+    """Handles all exceptions.
+
+    parameters:
+        exception (Exception): the exception object
+    """
+    code = getattr(exception, "code", 500)
+    return {"code": code, "error": str(exception)}, code
 
 
 app.run(host="0.0.0.0", port=81)
